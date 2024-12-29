8ae9385604f81b0c179531f8b0861b839846f215
Khalid9080 <khalidsaifullah9080@gmail.com>
2024-12-29 13:08:07 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
